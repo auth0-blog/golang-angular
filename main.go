246fbe71d9f6949442937ac861ba62a9b51d81eb
Bruno Krebs <bruno.krebs@auth0.com>
2018-08-10 17:48:32 -0300

    ### Building the Golang Web Server and Serving Static Files

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"path"
+	"path/filepath"
+	"github.com/pungyeon/golang-auth0-example/handlers"
+)
+
+func main() {
+	r := gin.Default()
+	r.NoRoute(func(c *gin.Context) {
+		dir, file := path.Split(c.Request.RequestURI)
+		ext := filepath.Ext(file)
+		if file == "" || ext == "" {
+			c.File("./ui/dist/ui/index.html")
+		} else {
+			c.File("./ui/dist/ui/" + path.Join(dir, file))
+		}
+	})
+
+	r.GET("/todo", handlers.GetTodoListHandler)
+	r.POST("/todo", handlers.AddTodoHandler)
+	r.DELETE("/todo/:id", handlers.DeleteTodoHandler)
+	r.PUT("/todo", handlers.CompleteTodoHandler)
+
+	err := r.Run(":3000")
+	if err != nil {
+		panic(err)
+	}
+}
